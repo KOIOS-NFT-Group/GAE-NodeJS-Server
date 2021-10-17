2287c3f450c4a81840e1254b3125d8d7013d1aa7
Leslie Lauw <32265116+LALauw@users.noreply.github.com>
2021-10-18 01:14:09 +0200

    added files

---
+++ index.js
@@ -0,0 +1,454 @@
+const express = require("express");
+const Moralis = require("moralis/node");
+const ipfs = require("ipfs-http-client");
+const Web3 = require("web3");
+var fs = require("fs");
+
+const app = express();
+const PORT = process.env.PORT || 1337;
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const privateKey = process.env.PRIVKEY;
+const ipfsClient = ipfs.create("https://ipfs.infura.io:5001");
+const abi = [
+  {
+    inputs: [
+      { internalType: "string", name: "_name", type: "string" },
+      { internalType: "string", name: "_symbol", type: "string" },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "approved",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "Approval",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "operator",
+        type: "address",
+      },
+      { indexed: false, internalType: "bool", name: "approved", type: "bool" },
+    ],
+    name: "ApprovalForAll",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "previousOwner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "OwnershipTransferred",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "address", name: "from", type: "address" },
+      { indexed: true, internalType: "address", name: "to", type: "address" },
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "Transfer",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "tokenID",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "address",
+        name: "minterAddress",
+        type: "address",
+      },
+    ],
+    name: "tokenMinted",
+    type: "event",
+  },
+  {
+    inputs: [],
+    name: "PRICE",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "uint256", name: "tokenId", type: "uint256" },
+      { internalType: "string", name: "_tokenURI", type: "string" },
+    ],
+    name: "_setTokenURI",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "tokenId", type: "uint256" },
+    ],
+    name: "approve",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "owner", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "balanceOfContract",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "tokenId", type: "uint256" }],
+    name: "burn",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getAllTokens",
+    outputs: [
+      {
+        components: [
+          { internalType: "uint256", name: "id", type: "uint256" },
+          { internalType: "string", name: "uri", type: "string" },
+        ],
+        internalType: "struct Kekw.KekwToken[]",
+        name: "",
+        type: "tuple[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "tokenId", type: "uint256" }],
+    name: "getApproved",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getBaseURI",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "owner", type: "address" },
+      { internalType: "address", name: "operator", type: "address" },
+    ],
+    name: "isApprovedForAll",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "mint",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "name",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "tokenId", type: "uint256" }],
+    name: "ownerOf",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "renounceOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "from", type: "address" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "tokenId", type: "uint256" },
+    ],
+    name: "safeTransferFrom",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "from", type: "address" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "tokenId", type: "uint256" },
+      { internalType: "bytes", name: "_data", type: "bytes" },
+    ],
+    name: "safeTransferFrom",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "operator", type: "address" },
+      { internalType: "bool", name: "approved", type: "bool" },
+    ],
+    name: "setApprovalForAll",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "string", name: "baseURI", type: "string" }],
+    name: "setBaseURI",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "bytes4", name: "interfaceId", type: "bytes4" }],
+    name: "supportsInterface",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "symbol",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "index", type: "uint256" }],
+    name: "tokenByIndex",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "owner", type: "address" },
+      { internalType: "uint256", name: "index", type: "uint256" },
+    ],
+    name: "tokenOfOwnerByIndex",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "tokenId", type: "uint256" }],
+    name: "tokenURI",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "totalSupply",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "from", type: "address" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "tokenId", type: "uint256" },
+    ],
+    name: "transferFrom",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "newOwner", type: "address" }],
+    name: "transferOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "_owner", type: "address" }],
+    name: "walletOfOwner",
+    outputs: [{ internalType: "uint256[]", name: "", type: "uint256[]" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "withdrawAll",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+];
+
+let image;
+let web3 = new Web3(
+  "https://rinkeby.infura.io/v3/8e4de63cfa6842e2811b357d94423d01"
+);
+let contract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
+let account = web3.eth.accounts.privateKeyToAccount("0x" + privateKey);
+
+Moralis.initialize(process.env.APP_ID, "", process.env.MASTER_KEY);
+Moralis.serverURL = process.env.SERVER_URL;
+
+app.listen(PORT, function () {
+  web3.eth.accounts.wallet.add(account);
+  web3.eth.defaultAccount = account.address;
+  contract.defaultChain = "rinkeby";
+  contract.defaultHardfork = "london";
+
+  init();
+});
+
+const getImage = () =>
+  fs.readFile("bart.png", function (err, data) {
+    if (data) {
+      image = data;
+      console.log(image);
+    }
+    if (err) {
+      console.log(err);
+    }
+  });
+
+const btoa = function (str) {
+  return Buffer.from(str).toString("base64");
+};
+
+init = async () => {
+  console.log("I have been summoned!");
+  let query = new Moralis.Query("KekwTokenEvents");
+  let subscription = await query.subscribe();
+  subscription.on("create", onTokenMint);
+  getImage();
+};
+
+onTokenMint = async (token) => {
+  try {
+    console.log("Minting... Token ID:" + token.attributes.tokenID);
+    lol(token.attributes.tokenID);
+  } catch (err) {
+    console.log("Error Occured: ");
+    console.log(err);
+  }
+};
+
+async function lol(tokenID) {
+  console.log("Metadata Minter has been summoned: ");
+  console.log("A token with ID: [" + tokenID + "] has been Minted.");
+  const gasLimit = await contract.methods
+    ._setTokenURI(
+      tokenID,
+      "https://ipfs.io/ipfs/Qmc31eQ8M68cEEukWYzynL2e56yEh5iJ3uXyYrwCVsSjxSasdasdad"
+    )
+    .estimateGas({
+      from: web3.eth.defaultAccount,
+    });
+
+  console.log("Gas For Function: " + gasLimit);
+  const latestBlock = await web3.eth.getBlock("latest");
+  const blockGas = latestBlock.gasLimit / 100000000000;
+  console.log("Gas For Gwei: " + blockGas + " ETH");
+
+  const addedImage = await ipfsClient.add(image);
+  console.log("Image Data: ");
+  console.log(addedImage);
+  const metadata = {
+    name: "Kekw",
+    description: "A collection of KEKWs",
+    image: "https://ipfs.io/ipfs/" + addedImage.path,
+    attributes: [{ type: "ID", value: tokenID }],
+  };
+  const addedMetadata = await ipfsClient.add(JSON.stringify(metadata));
+  console.log("Final Path: ");
+  console.log("https://ipfs.io/ipfs/" + addedMetadata.path);
+
+  const finalIpfs = addedMetadata.path;
+  contract.methods
+    ._setTokenURI(tokenID, finalIpfs)
+    .send({
+      from: web3.eth.defaultAccount,
+      gasLimit: gasLimit,
+    })
+    .on("transactionHash", function (hash) {
+      console.log(hash);
+    });
+}
